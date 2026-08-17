34a0723fbd66fdc702482a2275b7ef06125473cd
Graham-Holt <holt.grahams@gmail.com>
2026-08-16 22:03:49 -0400

    Improved rounding function: roundd()

--- Convenience/cof.m
+++ Convenience/cof.m
@@ -1,4 +1,15 @@
 function C = cof(M)
+% cof(M) computes cofactor matrices.
+% 
+% Graham Holt, August 2026. Updated August 2026
+% Embry-Riddle Aeronautical University
+% 
+%% Syntax
+% cof(M)
+% C = cof(M)
+% 
+%% Description
+% cof(M) returns the cofactor matrix as computed by cofactor determinants.
 
 n = size(M,1);
 
